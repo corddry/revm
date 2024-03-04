4ae1b591e9700bc29b5c7118a7b62b38ba833d5a
Jack Corddry <jackcorddry@gmail.com>
2024-03-04 12:45:27 -0800

    Naive implementation of Sha512

--- crates/precompile/src/hash.rs
+++ crates/precompile/src/hash.rs
@@ -10,6 +10,8 @@ pub const RIPEMD160: PrecompileWithAddress = PrecompileWithAddress(
     crate::u64_to_address(3),
     Precompile::Standard(ripemd160_run),
 );
+pub const SHA512: PrecompileWithAddress =
+    PrecompileWithAddress(crate::u64_to_address(1337), Precompile::Standard(sha512_run));
 
 /// See: <https://ethereum.github.io/yellowpaper/paper.pdf>
 /// See: <https://docs.soliditylang.org/en/develop/units-and-global-variables.html#mathematical-and-cryptographic-functions>
@@ -38,5 +40,15 @@ fn ripemd160_run(input: &Bytes, gas_limit: u64) -> PrecompileResult {
         let mut output = [0u8; 32];
         hasher.finalize_into((&mut output[12..]).into());
         Ok((gas_used, output.to_vec().into()))
+    }
+}
+
+fn sha512_run(input: &Bytes, gas_limit: u64) -> PrecompileResult {
+    let cost = calc_linear_cost_u32(input.len(), 60, 12);
+    if cost > gas_limit {
+        Err(Error::OutOfGas)
+    } else {
+        let output = sha2::Sha512::digest(input);
+        Ok((cost, output.to_vec().into()))
     }
 }
